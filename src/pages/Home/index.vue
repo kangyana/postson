7098bc2759a029dab728cc15c93fc23766d48480
kangyana <564469630@qq.com>
2020-11-08 22:12:48 +0800

    feat: layout

--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -1,5 +1,24 @@
 <template>
-  <div class="home">123</div>
+  <div class="home fixed-view flex">
+    <div class="history">
+      <div class="history-item flex" v-for="item in 20" :key="item">
+        <div class="request-type flex just-center align-center">GET</div>
+        <div class="flex-1">https://api.apiopen.top/getJoke?page=1&count=2&type=video</div>
+      </div>
+    </div>
+    <div class="request flex-1">
+      <div class="request-form flex">
+        <div class="request-search flex-1 flex">
+          <select class="request-select fw700">
+            <option value="GET">GET</option>
+            <option value="POST">POST</option>
+          </select>
+          <input class="request-input flex-1" />
+        </div>
+        <div class="request-button flex just-center align-center gesture--click disable-select">请 求</div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
